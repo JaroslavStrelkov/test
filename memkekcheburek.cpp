114bbf5460b9649db4bd46e19d20af609216f9ce
JaroslavStrelkov <jaroslaw.strelkovsb@gmail.com>
2024-10-24 10:35:16 +0300

    blabla

---
+++ memkekcheburek.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+int main(){
+    long double x, y, g, v, h,a=3;
+    x = '1' + '2' + '3';
+y = '1' - 49;
+g = 1 + bool(255);
+v = a++/2;
+    std::cout << x << "\t" << y << "\n";
+    std::cout <<  g << "\n";
+    std::cout << "v:" << v << "\n" << "h:" << h << "\n";
+
+
+
+
+
+
+    return 0;
+}
